35b22439ce19913b3d4803c3b13639e80868c845
Jesse <3191955858@qq.com>
2024-10-07 20:25:31 +0800

    SDL 开发模板代码头文件

---
+++ include/SDLTemplate.hpp
@@ -0,0 +1,12 @@
+#ifndef __SDLTEMPLATE_H_
+#define __SDLTEMPLATE_H_
+
+#include "./events.hpp"
+#include "./systemInit.hpp"
+#include "./texture.hpp"
+#include "./fmtTime.hpp"
+#include "./frameControl.hpp"
+#include "./audio.hpp"
+#include "./animation.hpp"
+
+#endif // __SDLTEMPLATE_H_
